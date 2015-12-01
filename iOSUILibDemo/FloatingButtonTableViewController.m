e76bdabc5b80adc7a1885dd48c03481eed1c0d8d
VuH-Uct <vuhoang.uct@gmail.com>
2015-12-01 16:27:34 +0700

    Update Floating buttons demo

--- iOSUILibDemo/FloatingButtonTableViewController.m
+++ iOSUILibDemo/FloatingButtonTableViewController.m
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-
 #import "FloatingButtonTableViewController.h"
 #import "FloatingActionButtonFlingingViewController.h"
 #import "FloatingButtonToolbarViewController.h"
@@ -36,86 +35,89 @@
 static NSArray *floatingButtonTypeName;
 
 + (NSArray *)floatingButtonTypeName {
-    if (!floatingButtonTypeName)
-        floatingButtonTypeName = [NSArray
-                                  arrayWithObjects:@"Transform into a toolbar", @"Flinging out related actions", @"Transforming into a single sheet", nil];
-    
-    return floatingButtonTypeName;
+  if (!floatingButtonTypeName)
+    floatingButtonTypeName =
+        [NSArray arrayWithObjects:@"Transform into a toolbar",
+                                  @"Flinging out related actions",
+                                  @"Transforming into a single sheet", nil];
+
+  return floatingButtonTypeName;
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    self.tableView.rowHeight = 44;
-    self.tableView.dataSource = self;
-    self.tableView.delegate = self;
-    
-    for (NSString *control in [FloatingButtonTableViewController floatingButtonTypeName]) {
-        [self insertRow:control];
-    }
-    [self.navigationController.navigationBar setTranslucent:NO];
+  [super viewDidLoad];
+
+  self.tableView.rowHeight = 44;
+  self.tableView.dataSource = self;
+  self.tableView.delegate = self;
+
+  for (NSString *control in
+       [FloatingButtonTableViewController floatingButtonTypeName]) {
+    [self insertRow:control];
+  }
+  [self.navigationController.navigationBar setTranslucent:NO];
+  self.title = @"Floating Button";
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+  [super didReceiveMemoryWarning];
 }
 
 - (void)insertRow:(NSString *)label {
-    if (!self.buttonTypes) {
-        self.buttonTypes = [[NSMutableArray alloc] init];
-    }
-    [self.buttonTypes addObject:label];
+  if (!self.buttonTypes) {
+    self.buttonTypes = [[NSMutableArray alloc] init];
+  }
+  [self.buttonTypes addObject:label];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+  return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.buttonTypes.count;
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section {
+  return self.buttonTypes.count;
 }
-
 
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    MDTableViewCell *cell =
-    [tableView dequeueReusableCellWithIdentifier:@"cell"];
-    if (cell == nil) {
-        // Create new MDTableViewCell with default style, or you can subclass it.
-        cell = [[MDTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                      reuseIdentifier:@"cell"];
-    }
-    
-    cell.textLabel.adjustsFontSizeToFitWidth = NO;
-    cell.textLabel.text = [self.buttonTypes objectAtIndex:indexPath.row];
-    
-    return cell;
+  MDTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+  if (cell == nil) {
+    // Create new MDTableViewCell with default style, or you can subclass it.
+    cell = [[MDTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                  reuseIdentifier:@"cell"];
+  }
+
+  cell.textLabel.adjustsFontSizeToFitWidth = NO;
+  cell.textLabel.text = [self.buttonTypes objectAtIndex:indexPath.row];
+
+  return cell;
 }
 
 - (void)tableView:(UITableView *)tableView
-didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSLog(@"did select row: %li:%li", (long)indexPath.section, (long)indexPath.row);
-    UIViewController *controller = nil;
+    didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  NSLog(@"did select row: %li:%li", (long)indexPath.section,
+        (long)indexPath.row);
+  UIViewController *controller = nil;
 
-    if (indexPath.row == 0) {
-        controller = [[FloatingButtonToolbarViewController alloc] init];
-    } else if (indexPath.row == 1) {
-        controller = [[FloatingActionButtonFlingingViewController alloc] init];
-    } else if (indexPath.row == 2) {
-        controller = [[FloatingButtonTranformingSheetViewController alloc] init];
-    }
-    
-    if (controller)
-        [self.navigationController pushViewController:controller animated:YES];
+  if (indexPath.row == 0) {
+    controller = [[FloatingButtonToolbarViewController alloc] init];
+  } else if (indexPath.row == 1) {
+    controller = [[FloatingActionButtonFlingingViewController alloc] init];
+  } else if (indexPath.row == 2) {
+    controller = [[FloatingButtonTranformingSheetViewController alloc] init];
+  }
+
+  if (controller)
+    [self.navigationController pushViewController:controller animated:YES];
 }
-
-
 
 /*
  // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath
+ *)indexPath {
  // Return NO if you do not want the specified item to be editable.
  return YES;
  }
@@ -123,25 +125,31 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 
 /*
  // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ - (void)tableView:(UITableView *)tableView
+ commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+ forRowAtIndexPath:(NSIndexPath *)indexPath {
  if (editingStyle == UITableViewCellEditingStyleDelete) {
  // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ [tableView deleteRowsAtIndexPaths:@[indexPath]
+ withRowAnimation:UITableViewRowAnimationFade];
  } else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ // Create a new instance of the appropriate class, insert it into the array,
+ and add a new row to the table view
  }
  }
  */
 
 /*
  // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath
+ *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
  }
  */
 
 /*
  // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath
+ *)indexPath {
  // Return NO if you do not want the item to be re-orderable.
  return YES;
  }
@@ -149,24 +157,29 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 
 /*
  #pragma mark - Table view delegate
- 
- // In a xib-based application, navigation from a table can be handled in -tableView:didSelectRowAtIndexPath:
- - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+ // In a xib-based application, navigation from a table can be handled in
+ -tableView:didSelectRowAtIndexPath:
+ - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath
+ *)indexPath {
  // Navigation logic may go here, for example:
  // Create the next view controller.
- <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:<#@"Nib name"#> bundle:nil];
- 
+ <#DetailViewController#> *detailViewController = [[<#DetailViewController#>
+ alloc] initWithNibName:<#@"Nib name"#> bundle:nil];
+
  // Pass the selected object to the new view controller.
- 
+
  // Push the view controller.
- [self.navigationController pushViewController:detailViewController animated:YES];
+ [self.navigationController pushViewController:detailViewController
+ animated:YES];
  }
  */
 
 /*
  #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
+
+ // In a storyboard-based application, you will often want to do a little
+ preparation before navigation
  - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
  // Get the new view controller using [segue destinationViewController].
  // Pass the selected object to the new view controller.
--- iOSUILibDemo/FloatingButtonToolbarViewController.m
+++ iOSUILibDemo/FloatingButtonToolbarViewController.m
@@ -17,173 +17,214 @@
 @implementation FloatingButtonToolbarViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+  [super viewDidLoad];
+  // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
--(void) viewWillLayoutSubviews
-{
-    
-    [super viewWillLayoutSubviews];
-    
+- (void)viewWillLayoutSubviews {
+
+  [super viewWillLayoutSubviews];
 }
 
--(void)viewDidLayoutSubviews {
-    UIView *mainView = [MDDeviceHelper getMainView];
-    self.viewToolbar.mdWidth = mainView.mdWidth;
-    self.viewToolbar.mdTop = mainView.mdHeight - self.viewToolbar.mdHeight - (self.navigationController.navigationBar.frame.size.height + self.navigationController.navigationBar.frame.origin.y);
-    
-    self.btShareToolbar.mdTop = self.viewToolbar.mdTop - self.btShareToolbar.mdHeight/2;
-    self.btShareToolbar.mdLeft = mainView.mdWidth - self.btShareToolbar.mdWidth - 10;
+- (void)viewDidLayoutSubviews {
+  UIView *mainView = [MDDeviceHelper getMainView];
+  self.viewToolbar.mdWidth = mainView.mdWidth;
+  self.viewToolbar.mdTop =
+      mainView.mdHeight - self.viewToolbar.mdHeight -
+      (self.navigationController.navigationBar.frame.size.height +
+       self.navigationController.navigationBar.frame.origin.y);
+
+  self.btShareToolbar.mdTop =
+      self.viewToolbar.mdTop - self.btShareToolbar.mdHeight / 2;
+  self.btShareToolbar.mdLeft =
+      mainView.mdWidth - self.btShareToolbar.mdWidth - 10;
 }
 
 - (IBAction)btShareClicked:(id)sender {
-    //show toolbar view
-    
-    CGFloat duration = 0.2f;
-    
-    //Animation type1: scale animation
-    //        self.viewToolbar.hidden = NO;
-    //        self.btShareToolbar.hidden = YES;
-    //        [self.viewToolbar setClipsToBounds:NO];
-    //        self.viewToolbar.transform = CGAffineTransformMakeScale(0.1,0.1);
-    //        [self.viewToolbar.layer setCornerRadius:0.f];
-    //
-    //        [self.viewToolbar.layer setMask:nil];
-    //        [UIView animateWithDuration:duration
-    //                              delay:0.0
-    //                            options: (UIViewAnimationOptionAllowUserInteraction|UIViewAnimationCurveEaseOut)
-    //                         animations:^{
-    //                             self.viewToolbar.alpha = 1;
-    //                             self.viewToolbar.transform = CGAffineTransformMakeScale(1.0,1.0);
-    //                         } completion:^(BOOL finished) {
-    //
-    //                         }];
-    
-    
-    //Animation type2: expand circle
-    CGPoint btShareToolbarCenter = self.btShareToolbar.center;
-    
+  // show toolbar view
+
+  CGFloat duration = 0.2f;
+
+  // Animation type1: scale animation
+  //        self.viewToolbar.hidden = NO;
+  //        self.btShareToolbar.hidden = YES;
+  //        [self.viewToolbar setClipsToBounds:NO];
+  //        self.viewToolbar.transform = CGAffineTransformMakeScale(0.1,0.1);
+  //        [self.viewToolbar.layer setCornerRadius:0.f];
+  //
+  //        [self.viewToolbar.layer setMask:nil];
+  //        [UIView animateWithDuration:duration
+  //                              delay:0.0
+  //                            options:
+  //                            (UIViewAnimationOptionAllowUserInteraction|UIViewAnimationCurveEaseOut)
+  //                         animations:^{
+  //                             self.viewToolbar.alpha = 1;
+  //                             self.viewToolbar.transform =
+  //                             CGAffineTransformMakeScale(1.0,1.0);
+  //                         } completion:^(BOOL finished) {
+  //
+  //                         }];
+
+  // Animation type2: expand circle
+  CGPoint btShareToolbarCenter = self.btShareToolbar.center;
+
+  [CATransaction begin];
+  [self.viewToolbar.layer removeAllAnimations];
+  [self.btShareToolbar.layer removeAllAnimations];
+
+  CAKeyframeAnimation *moveBtnShareToolbarAnimation =
+      [CAKeyframeAnimation animationWithKeyPath:@"position"];
+  moveBtnShareToolbarAnimation.calculationMode = kCAAnimationPaced;
+  moveBtnShareToolbarAnimation.fillMode = kCAFillModeForwards;
+  moveBtnShareToolbarAnimation.removedOnCompletion = NO;
+  CGPoint endPoint = self.viewToolbar.center;
+  CGMutablePathRef curvedPath = CGPathCreateMutable();
+  CGPathMoveToPoint(curvedPath, NULL, btShareToolbarCenter.x,
+                    btShareToolbarCenter.y);
+  CGPathAddCurveToPoint(curvedPath, NULL, btShareToolbarCenter.x, endPoint.y,
+                        btShareToolbarCenter.x, endPoint.y, endPoint.x,
+                        endPoint.y);
+  moveBtnShareToolbarAnimation.path = curvedPath;
+  moveBtnShareToolbarAnimation.duration = duration;
+  CGPathRelease(curvedPath);
+
+  [CATransaction setCompletionBlock:^{
     [CATransaction begin];
-    [self.viewToolbar.layer removeAllAnimations];
-    [self.btShareToolbar.layer removeAllAnimations];
-    
-    CAKeyframeAnimation *moveBtnShareToolbarAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
-    moveBtnShareToolbarAnimation.calculationMode = kCAAnimationPaced;
-    moveBtnShareToolbarAnimation.fillMode = kCAFillModeForwards;
-    moveBtnShareToolbarAnimation.removedOnCompletion = NO;
-    CGPoint endPoint = self.viewToolbar.center;
-    CGMutablePathRef curvedPath = CGPathCreateMutable();
-    CGPathMoveToPoint(curvedPath, NULL, btShareToolbarCenter.x, btShareToolbarCenter.y);
-    CGPathAddCurveToPoint(curvedPath, NULL, endPoint.x, btShareToolbarCenter.y, endPoint.x, btShareToolbarCenter.y, endPoint.x, endPoint.y);
-    moveBtnShareToolbarAnimation.path = curvedPath;
-    moveBtnShareToolbarAnimation.duration = duration;
-    CGPathRelease(curvedPath);
-    
-    
-    [CATransaction setCompletionBlock:^{
-        [CATransaction begin];
-        
-        self.viewToolbar.alpha = 1;
-        self.viewToolbar.hidden = NO;
-        self.btShareToolbar.hidden = YES;
-        
-        CAShapeLayer *aCircle=[CAShapeLayer layer];
-        CGFloat ratio = self.btShareToolbar.bounds.size.width/self.viewToolbar.bounds.size.width;
-        int circleLayerWidth = self.viewToolbar.bounds.size.width;
-        UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(self.viewToolbar.center.x - self.btShareToolbar.bounds.size.width/2, -(self.viewToolbar.bounds.size.width*ratio)/2 + self.viewToolbar.bounds.size.height/2, circleLayerWidth*ratio, circleLayerWidth*ratio)];
-        aCircle.path = path.CGPath;
-        aCircle.fillColor=[UIColor blackColor].CGColor;
-        [self.viewToolbar.layer setMask:aCircle];
-        
-        CGRect _rect = CGRectMake(0, - self.viewToolbar.bounds.size.width/2 + self.viewToolbar.bounds.size.height/2, circleLayerWidth, circleLayerWidth);
-        
-        CABasicAnimation *circleLayerAnimation = [CABasicAnimation animationWithKeyPath:@"path"];
-        circleLayerAnimation.toValue = (__bridge id)[UIBezierPath bezierPathWithOvalInRect:_rect].CGPath;
-        circleLayerAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-        circleLayerAnimation.autoreverses = NO;
-        circleLayerAnimation.repeatCount = 1;
-        circleLayerAnimation.duration = duration;
-        circleLayerAnimation.fillMode = kCAFillModeForwards;
-        circleLayerAnimation.removedOnCompletion = NO;
-        
-        [CATransaction setCompletionBlock:^{
-            [self.viewToolbar.layer setMask:nil];
-        }];
-        [aCircle addAnimation:circleLayerAnimation forKey:circleLayerAnimation.keyPath];
-        [CATransaction commit];
-    }];
-    
-    [self.btShareToolbar.layer addAnimation:moveBtnShareToolbarAnimation forKey:moveBtnShareToolbarAnimation.keyPath];
-    
-    [CATransaction commit];
 
-    
-}
+    self.viewToolbar.alpha = 1;
+    self.viewToolbar.hidden = NO;
+    self.btShareToolbar.hidden = YES;
 
-- (IBAction)mainViewTap:(id)sender {
-    if(self.viewToolbar.hidden) return;
-    //hide view toolbar
-    CGFloat duration = 0.2f;
-    CGPoint btShareToolbarCenter = self.btShareToolbar.center;
-    
-    CAShapeLayer *aCircle=[CAShapeLayer layer];
+    CAShapeLayer *aCircle = [CAShapeLayer layer];
+    CGFloat ratio = self.btShareToolbar.bounds.size.width /
+                    self.viewToolbar.bounds.size.width;
     int circleLayerWidth = self.viewToolbar.bounds.size.width;
-    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, -self.viewToolbar.bounds.size.width/2 + self.viewToolbar.bounds.size.height/2, circleLayerWidth, circleLayerWidth)];
+    UIBezierPath *path = [UIBezierPath
+        bezierPathWithOvalInRect:
+            CGRectMake(self.viewToolbar.center.x -
+                           self.btShareToolbar.bounds.size.width / 2,
+                       -(self.viewToolbar.bounds.size.width * ratio) / 2 +
+                           self.viewToolbar.bounds.size.height / 2,
+                       circleLayerWidth * ratio, circleLayerWidth * ratio)];
     aCircle.path = path.CGPath;
-    aCircle.fillColor=[UIColor blackColor].CGColor;
+    aCircle.fillColor = [UIColor blackColor].CGColor;
     [self.viewToolbar.layer setMask:aCircle];
-    
-    [CATransaction begin];
-    CGFloat ratio = self.btShareToolbar.bounds.size.width/self.viewToolbar.bounds.size.width;
-    
-    CGRect _rect = CGRectMake(self.viewToolbar.center.x - self.btShareToolbar.bounds.size.width/2, -(self.viewToolbar.bounds.size.width*ratio)/2 + self.viewToolbar.bounds.size.height/2, circleLayerWidth*ratio, circleLayerWidth*ratio);
-    
-    CABasicAnimation *circleLayerAnimation = [CABasicAnimation animationWithKeyPath:@"path"];
-    circleLayerAnimation.toValue = (__bridge id)[UIBezierPath bezierPathWithOvalInRect:_rect].CGPath;
-    circleLayerAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+
+    CGRect _rect = CGRectMake(0, -self.viewToolbar.bounds.size.width / 2 +
+                                     self.viewToolbar.bounds.size.height / 2,
+                              circleLayerWidth, circleLayerWidth);
+
+    CABasicAnimation *circleLayerAnimation =
+        [CABasicAnimation animationWithKeyPath:@"path"];
+    circleLayerAnimation.toValue =
+        (__bridge id)[UIBezierPath bezierPathWithOvalInRect:_rect].CGPath;
+    circleLayerAnimation.timingFunction = [CAMediaTimingFunction
+        functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
     circleLayerAnimation.autoreverses = NO;
     circleLayerAnimation.repeatCount = 1;
     circleLayerAnimation.duration = duration;
-    circleLayerAnimation.removedOnCompletion = false;
     circleLayerAnimation.fillMode = kCAFillModeForwards;
-    
+    circleLayerAnimation.removedOnCompletion = NO;
+
     [CATransaction setCompletionBlock:^{
-        [self.btShareToolbar.layer removeAllAnimations];
-        self.viewToolbar.hidden = YES;
-        self.btShareToolbar.hidden = NO;
-        [self.viewToolbar.layer removeAllAnimations];
-        
-        CAKeyframeAnimation *moveBtnShareToolbarAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
-        moveBtnShareToolbarAnimation.calculationMode = kCAAnimationPaced;
-        moveBtnShareToolbarAnimation.fillMode = kCAFillModeForwards;
-        moveBtnShareToolbarAnimation.removedOnCompletion = NO;
-        //Setting Endpoint of the animation
-        CGPoint endPoint = btShareToolbarCenter;
-        //to end animation in last tab use
-        CGMutablePathRef curvedPath = CGPathCreateMutable();
-        CGPathMoveToPoint(curvedPath, NULL, self.viewToolbar.center.x, self.viewToolbar.center.y);
-        CGPathAddCurveToPoint(curvedPath, NULL, endPoint.x, self.viewToolbar.center.y, endPoint.x, self.viewToolbar.center.y, endPoint.x, endPoint.y);
-        moveBtnShareToolbarAnimation.path = curvedPath;
-        moveBtnShareToolbarAnimation.duration = duration/2;
-        CGPathRelease(curvedPath);
-        [self.btShareToolbar.layer addAnimation:moveBtnShareToolbarAnimation forKey:moveBtnShareToolbarAnimation.keyPath];
+      [self.viewToolbar.layer setMask:nil];
     }];
-    
-    [aCircle addAnimation:circleLayerAnimation forKey:circleLayerAnimation.keyPath];
-    
+    [aCircle addAnimation:circleLayerAnimation
+                   forKey:circleLayerAnimation.keyPath];
     [CATransaction commit];
+  }];
+
+  [self.btShareToolbar.layer addAnimation:moveBtnShareToolbarAnimation
+                                   forKey:moveBtnShareToolbarAnimation.keyPath];
+
+  [CATransaction commit];
+}
+
+- (IBAction)mainViewTap:(id)sender {
+  if (self.viewToolbar.hidden)
+    return;
+  // hide view toolbar
+  CGFloat duration = 0.2f;
+  CGPoint btShareToolbarCenter = self.btShareToolbar.center;
+
+  CAShapeLayer *aCircle = [CAShapeLayer layer];
+  int circleLayerWidth = self.viewToolbar.bounds.size.width;
+  UIBezierPath *path = [UIBezierPath
+      bezierPathWithOvalInRect:CGRectMake(
+                                   0,
+                                   -self.viewToolbar.bounds.size.width / 2 +
+                                       self.viewToolbar.bounds.size.height / 2,
+                                   circleLayerWidth, circleLayerWidth)];
+  aCircle.path = path.CGPath;
+  aCircle.fillColor = [UIColor blackColor].CGColor;
+  [self.viewToolbar.layer setMask:aCircle];
+
+  [CATransaction begin];
+  CGFloat ratio = self.btShareToolbar.bounds.size.width /
+                  self.viewToolbar.bounds.size.width;
+
+  CGRect _rect = CGRectMake(self.viewToolbar.center.x -
+                                self.btShareToolbar.bounds.size.width / 2,
+                            -(self.viewToolbar.bounds.size.width * ratio) / 2 +
+                                self.viewToolbar.bounds.size.height / 2,
+                            circleLayerWidth * ratio, circleLayerWidth * ratio);
+
+  CABasicAnimation *circleLayerAnimation =
+      [CABasicAnimation animationWithKeyPath:@"path"];
+  circleLayerAnimation.toValue =
+      (__bridge id)[UIBezierPath bezierPathWithOvalInRect:_rect].CGPath;
+  circleLayerAnimation.timingFunction = [CAMediaTimingFunction
+      functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+  circleLayerAnimation.autoreverses = NO;
+  circleLayerAnimation.repeatCount = 1;
+  circleLayerAnimation.duration = duration;
+  circleLayerAnimation.removedOnCompletion = false;
+  circleLayerAnimation.fillMode = kCAFillModeForwards;
+
+  [CATransaction setCompletionBlock:^{
+    [self.btShareToolbar.layer removeAllAnimations];
+    self.viewToolbar.hidden = YES;
+    self.btShareToolbar.hidden = NO;
+    [self.viewToolbar.layer removeAllAnimations];
+
+    CAKeyframeAnimation *moveBtnShareToolbarAnimation =
+        [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    moveBtnShareToolbarAnimation.calculationMode = kCAAnimationPaced;
+    moveBtnShareToolbarAnimation.fillMode = kCAFillModeForwards;
+    moveBtnShareToolbarAnimation.removedOnCompletion = NO;
+    // Setting Endpoint of the animation
+    CGPoint endPoint = btShareToolbarCenter;
+    // to end animation in last tab use
+    CGMutablePathRef curvedPath = CGPathCreateMutable();
+    CGPathMoveToPoint(curvedPath, NULL, self.viewToolbar.center.x,
+                      self.viewToolbar.center.y);
+    CGPathAddCurveToPoint(curvedPath, NULL, endPoint.x,
+                          self.viewToolbar.center.y, endPoint.x,
+                          self.viewToolbar.center.y, endPoint.x, endPoint.y);
+    moveBtnShareToolbarAnimation.path = curvedPath;
+    moveBtnShareToolbarAnimation.duration = duration / 2;
+    CGPathRelease(curvedPath);
+    [self.btShareToolbar.layer
+        addAnimation:moveBtnShareToolbarAnimation
+              forKey:moveBtnShareToolbarAnimation.keyPath];
+  }];
+
+  [aCircle addAnimation:circleLayerAnimation
+                 forKey:circleLayerAnimation.keyPath];
+
+  [CATransaction commit];
 }
 
 /*
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
+// In a storyboard-based application, you will often want to do a little
+preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
